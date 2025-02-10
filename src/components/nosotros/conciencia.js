9171303e4f6db54ca1b510270eed541bafd0b268
Mauro <maurolobo.ml@gmail.com>
2025-02-10 17:16:22 -0300

    feat(about) 2  agrego la feat de about completa

---
+++ src/components/nosotros/conciencia.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './nosotros.css';
+
+function SustainabilityCircles() {
+  return (
+    <div className="flex justify-center items-center space-x-6 md:space-x-12">
+      {['Reducir', 'Reutilizar', 'Reciclar'].map((text) => (
+        <div
+          key={text}
+          className="md:w-60 md:h-60 w-24 h-24 flex items-center justify-center bg-gray-300 rounded-full text-center text-gray-700 font-medium transition-all duration-500 hover:animate-pulse"
+        >
+          {text}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default function Conciencia() {
+  return (
+    <div className="p-[var(--padding-generico-x-y)] hero">
+      <h2 className="text-[var(--text-grande)] text-center text-gray-800 font-bold">
+        CONCIENCIA AMBIENTAL
+      </h2>
+      {SustainabilityCircles()}
+    </div>
+  );
+}
---
+++ src/components/nosotros/hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './nosotros.css';
+
+export default function Hero() {
+  return (
+    <div className="p-[var(--padding-generico-x-y)] hero">
+      <h2>We are light</h2>
+      <h1 className="text-[var(--text-grande)] text-gray-800 font-bold">
+        CONOCÉ MÁS DE NUESTRA
+      </h1>
+      <h1 className="text-[var(--text-grande)] text-gray-800 font-bold">
+        EMPRESA
+      </h1>
+    </div>
+  );
+}
