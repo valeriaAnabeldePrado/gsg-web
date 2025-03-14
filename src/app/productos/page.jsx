44ecf91ce094e98718ae4a3c3c50036f357e5e75
valeriaAnabeldePrado <anabeltayu@gmail.com>
2025-03-14 15:36:15 -0300

    fix productos params

--- src/app/productos/page.jsx
+++ src/app/productos/page.jsx
@@ -7,25 +7,18 @@ import './productosSection.css';
 import LoaderP from '@/components/loader/loagerP';
 import FooterM from '@/components/footer/footer';
 
+// Componente contenedor que usa useSearchParams
 const ProductosContent = () => {
-  const searchParams = useSearchParams(); // Esto debe estar en un Suspense
+  const [originalProducts, setOriginalProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [loaderOk, setloaderOk] = useState(true);
+
+  const searchParams = useSearchParams();
   let categoria;
   if (searchParams.get('categoria')) {
     console.log(searchParams.get('categoria'));
     categoria = searchParams.get('categoria').toLowerCase();
   }
-
-  return (
-    <Suspense fallback={<LoaderP />}>
-      <ProductosList categoria={categoria} />
-    </Suspense>
-  );
-};
-
-const ProductosList = ({ categoria }) => {
-  const [originalProducts, setOriginalProducts] = useState([]);
-  const [products, setProducts] = useState([]);
-  const [loaderOk, setloaderOk] = useState(true);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -52,47 +45,50 @@ const ProductosList = ({ categoria }) => {
   }, [categoria, originalProducts]);
 
   return (
-    <div className="w-full wrapper-cont">
-      <section className="flex items-center justify-between flex-wrap w-full responsive-container">
-        {originalProducts && !loaderOk ? (
-          products.map((el, i) =>
-            el.modelos.map((modelo, j) =>
-              modelo.foto_portada ? (
-                <div
-                  key={`${i}_${j}_${modelo.id}`}
-                  className="container-itemss group transform transition-transform duration-300 group-hover:scale-100 rounded-3xl"
-                >
-                  <Link href={`/productos/${el.categoria}/${el._id}`}>
-                    <div className="relative container-img-gg transform transition-transform duration-300 group-hover:scale-100 rounded-3xl">
-                      <Image
-                        key={modelo.id}
-                        src={`${modelo.foto_portada}`}
-                        alt={modelo.subnombre}
-                        fill
-                        className="img-class rounded-3xl"
-                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                      />
-                      <div className="mask rounded-3xl"></div>
-                      <h2 className="title-gallery">{modelo.subnombre}</h2>
-                    </div>
-                  </Link>
-                </div>
-              ) : null,
-            ),
-          )
-        ) : (
-          <LoaderP />
-        )}
-      </section>
-    </div>
+    <>
+      <div className="w-full wrapper-cont">
+        <section className="flex items-center justify-between flex-wrap w-full responsive-container">
+          {originalProducts && !loaderOk ? (
+            products.map((el, i) =>
+              el.modelos.map((modelo, j) =>
+                modelo.foto_portada ? (
+                  <div
+                    key={`${i}_${j}_${modelo.id}`}
+                    className="container-itemss group transform transition-transform duration-300 group-hover:scale-100 rounded-3xl"
+                  >
+                    <Link href={`/productos/${el.categoria}/${el._id}`}>
+                      <div className="relative container-img-gg transform transition-transform duration-300 group-hover:scale-100 rounded-3xl">
+                        <Image
+                          key={modelo.id}
+                          src={`${modelo.foto_portada}`}
+                          alt={modelo.subnombre}
+                          fill
+                          className="img-class rounded-3xl"
+                          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        />
+                        <div className="mask rounded-3xl"></div>
+                        <h2 className="title-gallery">{modelo.subnombre}</h2>
+                      </div>
+                    </Link>
+                  </div>
+                ) : null,
+              ),
+            )
+          ) : (
+            <LoaderP />
+          )}
+        </section>
+      </div>
+      <FooterM />
+    </>
   );
 };
 
+// Componente principal que envuelve ProductosContent en Suspense
 const Productos = () => {
   return (
     <Suspense fallback={<LoaderP />}>
       <ProductosContent />
-      <FooterM />
     </Suspense>
   );
 };
