152adeb6c40166812609ab9a35c3e9eafb2ea2ed
valeriaAnabeldePrado <anabeltayu@gmail.com>
2025-03-14 15:45:25 -0300

    fix productos params

---
+++ src/app/productos/layout.jsx
@@ -0,0 +1,16 @@
+import FooterM from '@/components/footer/footer';
+import ListSelectedProducts from '@/components/list-selected-products/ListSelectedProducts';
+
+export const metadata = {
+  title: 'Productos',
+  description: 'Seccion de productos',
+};
+
+export default function ProductsLayout({ children }) {
+  return (
+    <div className="pt-10">
+      <ListSelectedProducts />
+      {children}
+    </div>
+  );
+}
---
+++ src/app/productos/page.jsx
@@ -0,0 +1,95 @@
+'use client';
+import React, { useEffect, useState, Suspense } from 'react';
+import Image from 'next/image';
+import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import './productosSection.css';
+import LoaderP from '@/components/loader/loagerP';
+import FooterM from '@/components/footer/footer';
+
+const ProductosContent = () => {
+  const [originalProducts, setOriginalProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [loaderOk, setloaderOk] = useState(true);
+
+  const searchParams = useSearchParams();
+  let categoria;
+  if (searchParams.get('categoria')) {
+    console.log(searchParams.get('categoria'));
+    categoria = searchParams.get('categoria').toLowerCase();
+  }
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch(`/api/products`);
+      const data1 = await res.json();
+      const data = data1.products;
+      setOriginalProducts(data);
+      setloaderOk(false);
+    };
+
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    if (categoria === 'todos' || !categoria) {
+      setProducts(originalProducts);
+    } else {
+      const filteredProducts = originalProducts.filter(
+        (product) => product.categoria.toLowerCase() === categoria,
+      );
+
+      setProducts(filteredProducts);
+    }
+  }, [categoria, originalProducts]);
+
+  return (
+    <>
+      <div className="w-full wrapper-cont">
+        <section className="flex items-center justify-between flex-wrap w-full responsive-container">
+          {originalProducts && !loaderOk ? (
+            products.map((el, i) =>
+              el.modelos.map((modelo, j) =>
+                modelo.foto_portada ? (
+                  <div
+                    key={`${i}_${j}_${modelo.id}`}
+                    className="container-itemss group transform transition-transform duration-300 group-hover:scale-100 rounded-3xl"
+                  >
+                    <Link href={`/productos/${el.categoria}/${el._id}`}>
+                      <div className="relative container-img-gg transform transition-transform duration-300 group-hover:scale-100 rounded-3xl">
+                        <Image
+                          key={modelo.id}
+                          src={`${modelo.foto_portada}`}
+                          alt={modelo.subnombre}
+                          fill
+                          className="img-class rounded-3xl"
+                          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        />
+                        <div className="mask rounded-3xl"></div>
+                        <h2 className="title-gallery">{modelo.subnombre}</h2>
+                      </div>
+                    </Link>
+                  </div>
+                ) : null,
+              ),
+            )
+          ) : (
+            <LoaderP />
+          )}
+        </section>
+      </div>
+      <FooterM />
+    </>
+  );
+};
+
+// Componente principal que envuelve ProductosContent en Suspense
+const Productos = () => {
+  return (
+    <Suspense fallback={<LoaderP />}>
+      <ProductosContent />
+    </Suspense>
+  );
+};
+
+export default Productos;
